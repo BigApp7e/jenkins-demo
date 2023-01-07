2c3d0ba6a9d6369bdf71549b5fc8392989268657
Stoycho Stankov <stoychos@ext.inditex.com>
2023-01-07 19:26:12 +0200

    add post construct right import

--- src/main/java/core/JenkinsDemoApplication.java
+++ src/main/java/core/JenkinsDemoApplication.java
@@ -1,10 +1,12 @@
 package core;
 
-import jakarta.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {

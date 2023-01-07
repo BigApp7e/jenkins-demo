4cb5ef498c462b557a58b737fd288c44eb8b1c7b
Stoycho Stankov <stoychos@ext.inditex.com>
2023-01-07 19:15:38 +0200

    add logger

--- src/main/java/core/JenkinsDemoApplication.java
+++ src/main/java/core/JenkinsDemoApplication.java
@@ -1,12 +1,23 @@
 package core;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
 
+	public static Logger LOGGER = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	@PostConstruct
+	public void init(){
+		LOGGER.info("Application started ...");
+	}
+
 	public static void main(String[] args) {
+		LOGGER.info("Application executed ...");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
--- src/test/java/core/JenkinsDemoApplicationTests.java
+++ src/test/java/core/JenkinsDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsDemoApplicationTests {
 
+	public static Logger LOGGER = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		LOGGER.info("Execute test ...");
+		Assertions.assertEquals(true, true);
 	}
 
 }
